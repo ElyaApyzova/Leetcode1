aae1132ee7b3180092432766ce6d3e497a81fb8e
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-10-19 23:05:35 +0300

    completed

--- app.js
+++ app.js
@@ -595,4 +595,41 @@ class Solution {
 }
 
 
+//273 https://leetcode.com/problems/integer-to-english-words/
+
+//Input: num = 123
+//Output: "One Hundred Twenty Three"
+
+
+const numberToWords = function(num) {
+  if (num === 0) return "Zero";
+
+  const belowTwenty = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"];
+
+  const tens = ["", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"];
+  const thousands = ["", "Thousand", "Million", "Billion"];
+
+  let result = "";
+  let i = 0;
+
+  while (num > 0) {
+    if (num % 1000 !== 0) {
+      result = helper(num % 1000) + thousands[i] + " " + result;
+    }
+    num = Math.floor(num / 1000);
+    i++;
+  }
+  return result.trim();
+};
+
+function helper(num) {
+  const belowTwenty = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"];
+  const tens = ["", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"];
+
+  if (num === 0) return "";
+  else if (num < 20) return belowTwenty[num] + " ";
+  else if (num < 100) return tens[Math.floor(num / 10)] + " " + helper(num % 10);
+  else return belowTwenty[Math.floor(num / 100)] + "  Hundred " + helper(num % 100);
+}
+
 
