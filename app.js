8a9a74e1c88cdeda6012db61f9712cdd631c6a27
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-10-23 22:44:31 +0300

    completed

--- app.js
+++ app.js
@@ -719,5 +719,23 @@ const combinationSum3 = function(k, n) {
 
 
 
+const HIndex = function(citations) {
+  let n = citations.length;
+  let left = 0, right = n - 1;
+
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+    if (citations[mid] === n - mid) {
+      return n - mid;
+    } else if (citations[mid] < n - mid) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return n - left;
+};
+
+
 
 
