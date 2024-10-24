2e8e87334286c89333974a00929c5947ab3c6d80
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-10-24 23:20:10 +0300

    completed

--- app.js
+++ app.js
@@ -826,3 +826,19 @@ const numWays = function(n, k) {
 
   return onePostBack;
 };
+
+219. //https://leetcode.com/problems/contains-duplicate-ii/
+
+//Input: nums = [1,2,3,1,2,3], k = 2
+//Output: false
+
+const containsNearbyDuplicate = function(nums, k) {
+  let set = new Set();
+  for (let i = 0; i < nums.length; ++i) {
+    if (set.has(nums[i])) return true;
+    set.add(nums[i]);
+    if (set.size > k) set.delete(nums[i - k]);
+  }
+  return false;
+};
+
